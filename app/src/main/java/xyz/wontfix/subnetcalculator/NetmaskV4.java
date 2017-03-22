ff271075c3be6ef71ce81441f7da56d42900026e
Eliott <eliott.neviere@gmail.com>
2017-03-22 10:30:11 +0100

    Added isAValidBinaryNetmask method in NetmaskV4 class.

--- app/src/main/java/xyz/wontfix/subnetcalculator/NetmaskV4.java
+++ app/src/main/java/xyz/wontfix/subnetcalculator/NetmaskV4.java
@@ -6,6 +6,7 @@ package xyz.wontfix.subnetcalculator;
 
 public class NetmaskV4 {
     public ByteV4[] fields;
+    public int suffix;
 
     public NetmaskV4 () {
         fields = new ByteV4[4];
@@ -13,5 +14,27 @@ public class NetmaskV4 {
         fields[1] = new ByteV4();
         fields[2] = new ByteV4();
         fields[3] = new ByteV4();
+    }
+
+    public boolean isAValidBinaryNetmask() {
+
+        boolean isValid = true;
+        boolean breakpoint = false;
+        int field = 3;
+        int bit = 7;
+
+        while (field >= 0 && isValid) {
+            while (bit >= 0 && isValid) {
+                if (fields[field].binaryValue[bit] == 0 && !breakpoint) {
+                        breakpoint = true;
+                }
+                if (fields[field].binaryValue[bit] == 1 && breakpoint) {
+                        isValid = false;
+                }
+                --bit;
+            }
+            --field;
+        }
+        return isValid;
     }
 }
