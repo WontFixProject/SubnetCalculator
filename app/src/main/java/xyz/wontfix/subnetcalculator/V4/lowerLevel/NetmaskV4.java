9cf8a34e0c5fc1af467197b616ccb728e6eee79e
Eliott <eliott.neviere@gmail.com>
2017-03-22 19:37:38 +0100

    Fixed code error in setPrefixFromBinary method in NetmaskV4 class.

--- app/src/main/java/xyz/wontfix/subnetcalculator/V4/lowerLevel/NetmaskV4.java
+++ app/src/main/java/xyz/wontfix/subnetcalculator/V4/lowerLevel/NetmaskV4.java
@@ -160,10 +160,11 @@ public class NetmaskV4 extends AddressV4 {
 
         boolean breakpoint = false;
         int field = 3;
-        int bit = 7;
+        int bit;
         int prefix = 0;
 
         while (field >= 0 && !breakpoint) {
+            bit = 7;
             while (bit >= 0 && !breakpoint) {
                 if (bytes[field].getBinaryValue()[bit] == 1)
                     ++prefix;
