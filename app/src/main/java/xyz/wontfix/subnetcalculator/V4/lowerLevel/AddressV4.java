583be06ba6bfdef88a30661832fd5eaac15faba3
Eliott <eliott.neviere@gmail.com>
2017-03-23 11:34:04 +0100

    Added AddressV4 comments and styles.

--- app/src/main/java/xyz/wontfix/subnetcalculator/V4/lowerLevel/AddressV4.java
+++ app/src/main/java/xyz/wontfix/subnetcalculator/V4/lowerLevel/AddressV4.java
@@ -15,63 +15,92 @@
 
 package xyz.wontfix.subnetcalculator.V4.lowerLevel;
 
+/*
+ * This class represents IPv4 addresses. It has four bytes and a number of methods
+ * setting and getting these bytes while checking the integrity and consistency of
+ * data inputs.
+ */
 public class AddressV4 {
 
-                                                /*
-##############################################################################################
-# =========================================[ FIELDS ] ====================================== #
-##############################################################################################
-                                               */
+
+    //############################################################################################//
+    // =========================================[ FIELDS ] ====================================== //
+    //############################################################################################//
+
     ByteV4[] bytes;
 
-                                                /*
-##############################################################################################
-# ======================================[ CONSTRUCTORS ] =================================== #
-##############################################################################################
-                                                */
+
+    //############################################################################################//
+    // ======================================[ CONSTRUCTORS ] =================================== //
+    //############################################################################################//
+
     public AddressV4 () {
-        bytes = new ByteV4[4];
-        bytes[0] = new ByteV4();
-        bytes[1] = new ByteV4();
-        bytes[2] = new ByteV4();
-        bytes[3] = new ByteV4();
+        bytes = new ByteV4[4];      //IPV4 addresses have four bytes
+        bytes[0] = new ByteV4();    //Right byte
+        bytes[1] = new ByteV4();    //Middle-right byte
+        bytes[2] = new ByteV4();    //Middle-left byte
+        bytes[3] = new ByteV4();    //Left byte
     }
 
-    /*
-##############################################################################################
-# ======================================[ GETORS/SETORS ] ================================== #
-##############################################################################################
-    */
 
-    public ByteV4 getByte(int index) {
-        return bytes[index];
+    //############################################################################################//
+    // ======================================[ GETORS/SETORS ] ================================== //
+    //############################################################################################//
+
+    /**
+     * Return one of the four ByteV4
+     * @param field provides the array position of the desired byte to be returned.
+     * @return the requested byte.
+     */
+    public ByteV4 getByte(int field) {
+        return bytes[field];
     }
 
+    /**
+     * Set the value of one byte with a decimal value.
+     * This method check if the input is valid beforehand
+     * and return whether the operation has been successful or not.
+     * @param field provides the array position of the desired byte to be set.
+     * @param decimalValue provides the decimal value for the byte to set.
+     * @return true if the input is valid and the operation has been successful. False otherwise.
+     */
     public boolean setByteByDecimal(int field, int decimalValue) {
         return (isAValidDecimalValue(decimalValue) && bytes[field].setDecimalValue(decimalValue));
     }
 
+    /**
+     * Set the value of one byte with a binary value.
+     * This method check if the input is valid beforehand
+     * and return whether the operation has been successful or not.
+     * @param field provides the array position of the desired byte to be set.
+     * @param binaryValue provides the binary value for the byte to set.
+     * @return true if the input is valid and the operation has been successful. False otherwise.
+     */
     public boolean setByteByBinary(int field, int[] binaryValue) {
         return (isAValidBinaryValue(binaryValue) && bytes[field].setBinaryValue(binaryValue));
     }
 
-                                                /*
-##############################################################################################
-# ======================================[ PUBLIC METHODS ] ================================= #
-##############################################################################################
-                                                */
 
+    //############################################################################################//
+    // =====================================[ PRIVATE METHODS ] ================================= //
+    //############################################################################################//
 
-                                                /*
-##############################################################################################
-# =====================================[ PRIVATE METHODS ] ================================= #
-##############################################################################################
-                                                */
-
+    /**
+     * Check if a decimal value is a suitable candidate for a IPV4 address byte.
+     * Value must be equal or greater than 0 and equal or lower than 255.
+     * @param decimalValue decimal value to be checked.
+     * @return true if the given decimal value is valid.
+     */
     private boolean isAValidDecimalValue(int decimalValue) {
         return (decimalValue >= 0 && decimalValue <= 255);
     }
 
+    /**
+     * Check if a binary value is a suitable candidate for a IPV4 address byte.
+     * Value must not be a null array and have a length of exactly 8.
+     * @param binaryValue decimal value to be checked.
+     * @return true if the given binary value (array) is valid.
+     */
     private boolean isAValidBinaryValue(int[] binaryValue) {
         return (binaryValue != null && binaryValue.length == 8);
     }
